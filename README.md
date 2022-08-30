e5b6b2c542acaf204e5009c8c6e3f33bc6774e92
OmerutaPro <omerutapro@MacBook-Pro-de-OmerutaPro.local>
2022-08-30 23:36:36 +0200

    javasript menu done. responsive check done for mobile, desktop and tablet

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # landing-page-2022
-https://fonts.google.com/specimen/Source+Sans+Pro?query=source+sa
+
+<!-- https://fonts.google.com/specimen/Source+Sans+Pro?query=source+sa -->
+
+Google Font Source Sans Pro
+<!-- 'https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&family=Source+Sans+Pro:wght@300;400;600;700&family=Syne:wght@400;600;700&display=swap'); -->
 
--- js/app.js
+++ js/app.js
@@ -38,13 +38,77 @@
  * 
 */
 
+// Mobile Navigation
+
+
+
+/*const navBtn = document.querySelector('.mobile-nav-btn');
+const navLink = document.querySelector('.navbar-mobile-link');
+
+navBtn.addEventListener('click', () => {
+    navLink.classList.toggle('hide');
+} );*/
+
+
+
+
 // build the nav
+
+// const menu = ["Home", "Showcase", "How we do it", "Our projrcts", "Testimonials", Subscribe]
+
+const sections = document.querySelectorAll('section');
+const navBar = document.querySelector('#navbar-list');
+// const navBarContent = '';
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 0; i < sections.length; i++)
+{   
+    let sectionId = sections[i].id;    
+    const navLink = document.createElement('li');
+    navLink.innerHTML = `<a href="#${sectionId}" class="menu-link active">${sectionId}</a>`;
+    navBar.appendChild(navLink);
+
+}
+} );
+
+
+
+
+
+
+
+
+
 
 
 // Add class 'active' to section when near top of viewport
 
+// const navLink = document.querySelectorAll('.menu-link');
+// navLink.forEach(link => {
+//     link.addEventListener('click', () => {
+//         navLink.forEach(link => {
+//             link.classList.remove('active');
+//         });
+//         link.classList.add('active');
+//     });
+// });
+
 
 // Scroll to anchor ID using scrollTO event
+
+// const navLink = scrollTo.querySelectorAll('.menu-link');
+// navLink.forEach(link => {
+//     link.addEventListener('click', () => {
+//         navLink.forEach(link => {
+//             link.classList.remove('active');
+//         } );
+
+//         link.classList.add('active');
+//         const sectionId = link.getAttribute('href');
+//         const section = document.querySelector(sectionId);
+//         section.scrollIntoView({behavior: 'smooth'});
+//     } );
+// } );
+
 
 
 /**
@@ -54,6 +118,8 @@
 */
 
 // Build menu 
+
+
 
 // Scroll to section on link click
 
